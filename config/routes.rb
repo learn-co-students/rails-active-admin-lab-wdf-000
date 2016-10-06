54e2b23d217ea54b023d1fab5841cd88f47c6bfb
Derick Quartey <niikwartey1@gmail.com>
2016-10-06 10:26:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  # resources :artists, :songs
+
+  #expect(get: '/artists/new').to route_to('artists#show', id: "new")
+  get '/artists/:id', to: 'artists#show'
+  get '/songs/:id', to: 'songs#show'
 
   root to: 'artists#index'
 end
