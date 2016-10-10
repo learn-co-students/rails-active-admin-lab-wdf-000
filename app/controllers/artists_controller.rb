5ebeec1672f19fb0b1b675f23ec8c75bb5dac513
satub <satu.barnhill@gmail.com>
2016-10-10 10:43:22 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,46 +1,48 @@
 class ArtistsController < ApplicationController
-  def index
-  end
+
+  # def index
+  # end
 
   def show
   end
 
-  def new
-    @artist = Artist.new
-  end
+  # def new
+  #
+  #   @artist = Artist.new
+  # end
 
-  def create
-    @artist = Artist.new(artist_params)
+  # def create
+  #   @artist = Artist.new(artist_params)
+  #
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :new
+  #   end
+  # end
 
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
-    end
-  end
+  # def edit
+  #   @artist = Artist.find(params[:id])
+  # end
+  #
+  # def update
+  #   @artist = Artist.find(params[:id])
+  #
+  #   @artist.update(artist_params)
+  #
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def edit
-    @artist = Artist.find(params[:id])
-  end
-
-  def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
+  # def destroy
+  #   @artist = Artist.find(params[:id])
+  #   @artist.destroy
+  #   flash[:notice] = "Artist deleted."
+  #   redirect_to artists_path
+  # end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
 
-  root to: 'artists#index'
+  resources :artists, only: [:show]
+  resources :songs, only: [:show, :index]
+
+  root to: 'songs#index'
 end
