f44e118daff7a782569c7d647a94b8458df0237d
Bibi Khadiza <limakhadiza@yahoo.com>
2016-11-08 14:54:09 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  resources :artists, only: [:show, :index]
+  resources :songs, only: [:show, :index]
 
   root to: 'artists#index'
 end
