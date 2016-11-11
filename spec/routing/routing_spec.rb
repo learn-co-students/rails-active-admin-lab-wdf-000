e588491f348232c63fbdd31ecf1fb1272b512ce5
Isuru <github email address>
2016-11-11 13:27:39 -0500

    Done.

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "routing" do
   it "doesn't route artist create and edit routes" do
-    expect(get: '/artists/new').to route_to('artists#show', id: "new")
-    expect(get: '/artists/edit').to route_to('artists#show', id: "edit")
+    expect(get: '/artists/new').to route_to('artists#show')
+    expect(get: '/artists/edit').to route_to('artists#show')
     expect(post: '/artists').to_not be_routable
     expect(patch: '/artists/update').to_not be_routable
   end
 
   it "doesn't route song create and edit routes" do
-    expect(get: '/songs/new').to route_to('songs#show', id: "new")
-    expect(get: '/songs/edit').to route_to('songs#show', id: "edit")
+    expect(get: '/songs/new').to route_to('songs#show')
+    expect(get: '/songs/edit').to route_to('songs#show')
     expect(post: '/artists').to_not be_routable
     expect(patch: '/artists/update').to_not be_routable
   end
